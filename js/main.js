c941483caa0c8a13bf5cce0091f84d002c24dc53
Contrigra <gaponov60b@gmail.com>
2023-10-16 14:28:50 +0300

    feat(player): basic current player render

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const buttonContainer = document.querySelector('.button-container');
 const resetButton = document.querySelector('.button-reset-game');
-
+const playerContainer = document.querySelector('.player-container');
 window.onload = () => {
     screenController.updateScreen();
 }
@@ -13,27 +13,42 @@ const screenController = function () {
     const renderManyButtons = () => {
         // remove all elements
         buttonContainer.replaceChildren()
-        for (let buttonValue of gameController.gameBoard) {
-            renderOneButton(buttonValue);
+        for (let [buttonIndex, buttonValue] of gameController.gameBoard.entries()) {
+            renderOneButton(buttonIndex, buttonValue);
         }
     }
 
-    const renderOneButton = (buttonValue) => {
+    const renderOneButton = (buttonIndex, buttonValue) => {
         const gameButton = document.createElement('button');
-        gameButton.textContent = (buttonValue);
+        gameButton.textContent = buttonValue;
         gameButton.setAttribute(`data-value`, `${buttonValue}`);
+        // TODO save index within the button
+        gameButton.setAttribute(`data-index`, `${buttonIndex}`);
+
         gameButton.classList.add('button-pad')
         buttonContainer.appendChild(gameButton);
     }
 
-    const updateScreen = () => renderManyButtons();
+    const updatePlayer = () => {
+        playerContainer.textContent = gameController.currentPlayer
+
+    }
+    const updateScreen = () => {
+        renderManyButtons();
+        updatePlayer();
+    }
+
+
     return {updateScreen};
 }();
+
 const gameController = function () {
     const gameBoard =
         ['x', 'x', 'o',
             'x', 'o', 'o',
             'o', 'x', 'x'];
+
+    const currentPlayer = 'Player X'
 
     function resetGame() {
         this.gameBoard = ['', '', '',
@@ -41,13 +56,29 @@ const gameController = function () {
             '', '', '']
     }
 
-    return {gameBoard, resetGame};
+    function changePlayer() {
+        this.currentPlayer = this.currentPlayer === 'Player X' ? 'Player O' : 'Player X';
+    }
+
+    return {gameBoard, resetGame, currentPlayer, changePlayer};
+
 }();
 
+/* TODO
+*   1. Proclaim a victor
+    - Game controller contains the value (empty by default)
+    * Create a dummy function to check a victor that calls screenController.updateVictor()
+    - If victor is not null call screenController.updateVictor()
+    * reset should set victor value to null
+    2. Responsive Buttons
+    * Click on a button should change it's value corresponding to the player.
+    * Reflect changes in gameController.gameBoard
+    * Each click should trigger gameController.checkForVictor()
+    3. Determine a victor.
+    - When you get a row, then you're the victor.
+    *
+ */
+
+// TODO Possibly change to store only O or X gameController.currentPlayer
 
 
-console.log(gameController.gameBoard)
-// console.log(gameController.resetGame())
-console.log(gameController.gameBoard)
-gameController.sayWinner = () => console.log('Winner is you!')
-console.log(gameController)
